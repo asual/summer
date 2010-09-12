bb701629649aa79275d9c630bbcc9a092c61c0c9
rgeorgiev <rgeorgiev@gmail.com>
2010-09-12 13:45:04 +0300

    explicitly register ResourceEditorRegistrar to support by default String to Resources converting

---
+++ core/src/main/java/com/asual/summer/core/spring/ResourceEditorRegistrarFactoryBean.java
@@ -0,0 +1,37 @@
+package com.asual.summer.core.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.support.ResourceEditorRegistrar;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * @author rgeorgiev
+ *
+ */
+public class ResourceEditorRegistrarFactoryBean implements FactoryBean<ResourceEditorRegistrar>,ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public ResourceEditorRegistrar getObject() throws Exception {
+        return new ResourceEditorRegistrar(applicationContext);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return ResourceEditorRegistrar.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext=applicationContext;
+    }
+
+}
