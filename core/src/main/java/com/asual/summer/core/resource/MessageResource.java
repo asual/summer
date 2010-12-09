50ac4c66afdfc1909ef948db3bd9412b5f28db03
asual <rostislav@asual.com>
2010-12-09 18:39:51 +0200

    Adds support for extra classpath resources.

--- core/src/main/java/com/asual/summer/core/resource/MessageResource.java
+++ core/src/main/java/com/asual/summer/core/resource/MessageResource.java
@@ -67,7 +67,7 @@ public class MessageResource extends AbstractResource {
 						URL url = resource.getURL();
 						boolean isJar = ResourceUtils.isJarURL(url);
 						String basename = "classpath:" + url.getFile()
-							.split(isJar ? ResourceUtils.JAR_URL_SEPARATOR : "/classes/")[1]
+							.split(isJar ? ResourceUtils.JAR_URL_SEPARATOR : "/(classes|resources)/")[1]
 							.replaceAll("(_\\w+){0,3}\\.(properties|xml)", "");
 						if (!basenames.contains(basename)) {
 							if (isJar) {
